c26a50405a46dd4e0fd6c820e623b16f712044d1
iamakj15 <abhishekkjju@gmail.com>
2021-06-08 20:06:17 +0530

    2nd commit

---
+++ src/charisalphabetornot.java
@@ -0,0 +1,9 @@
+public class charisalphabetornot {
+    public static void main(String[] args) {
+        char ch = '$';
+        if (Character.isAlphabetic(ch))
+        System.out.println("aplabet");
+        else
+        System.out.println("not an alphabet");
+    }
+}
