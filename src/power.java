721fe2b9d81851e908c58b59ab12433b3c0fea9d
iamakj15 <abhishekkjju@gmail.com>
2021-06-09 22:09:44 +0530

    3rd commit

---
+++ src/power.java
@@ -0,0 +1,13 @@
+public class power {
+    public static void main(String[] args) {
+        int b = 5;
+        int e = 4;
+        int t = 1;
+        while(e!=0){
+            t *= b;
+            --e;
+        }
+        System.out.println(t);
+
+    }
+}
