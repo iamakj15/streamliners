3e06858832c042016dc8d4844b0a946d14749fa1
iamakj15 <abhishekkjju@gmail.com>
2021-06-09 21:40:01 +0530

    3rd commit

---
+++ src/sumofnat.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class sumofnat {
+    public static void main(String[] args) {
+        Scanner s1 = new Scanner(System.in);
+        System.out.println("enter a no.");
+        int g = s1.nextInt();
+        int first = 1;int sum = 0;
+        for(int i=1;i<g;i++){
+            sum +=i;
+
+    } System.out.println(sum);
+}}
