7ead1a003aa9338748b1244dfdbd8d34d6b87794
iamakj15 <abhishekkjju@gmail.com>
2021-06-16 21:37:12 +0530

    4th commit

---
+++ src/iterate.java
@@ -0,0 +1,9 @@
+public class iterate {
+    public static void main(String[] args) {
+        String j = "abhi";
+        for(int i =0;i<j.length();i++){
+            char a = j.charAt(i);
+
+        System.out.print(a + ",");
+    }
+}}
