6fbb43696e4f88c583716918196bba6d052c05ca
iamakj15 <abhishekkjju@gmail.com>
2021-06-16 20:55:20 +0530

    4th commit

---
+++ src/comparestrings.java
@@ -0,0 +1,10 @@
+public class comparestrings {
+    public static void main(String[] args) {
+        String a = "abhi";
+        String b = "Abhi";
+        if(a.equals(b))
+            System.out.println("equal");
+        else
+            System.out.println("not equal");
+    }
+}
